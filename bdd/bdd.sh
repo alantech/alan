77e044c6f86edd05e927c11f76c759e56e769c18
David Ellis <isv.damocles@gmail.com>
2020-05-14 17:12:25 -0700

    Remove some of the debugging changes.

--- bdd/bdd.sh
+++ bdd/bdd.sh
@@ -27,7 +27,6 @@ PHYS_DIR=`pwd -P`
 
 PATH="${PATH}:${PHYS_DIR}:${PHYS_DIR}/../shellspec:${PHYS_DIR}/../node_modules/.bin:${PHYS_DIR}/../runtime/target/release:${PHYS_DIR}/../interpreter"
 export PATH
-echo $PATH
-bash -c "shellspec -s /bin/bash"
+shellspec -s /bin/bash
 
 exit $?
