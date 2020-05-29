f9214fbeaba45564e056f917ea333152a551e1f3
David Ellis <isv.damocles@gmail.com>
2020-05-29 14:24:37 -0700

    Manually running the compiler on this worked, but it failed during BDD tests

--- compiler/src/lntoamm/Std.js
+++ compiler/src/lntoamm/Std.js
@@ -21,8 +21,45 @@ module.exports = {
         Module.getAllModules()['<root>'] = rootModule
       }
     })
+    // Put the remaining ASTs in a loadable order
+    const orderedAsts = []
+    let i = 0
+    while (stdAsts.length > 0) {
+      const stdAst = stdAsts[i]
+      // Just remove the root node, already processed
+      if (stdAst.name === 'root.ln') {
+        stdAsts.splice(i, 1)
+        continue
+      }
+      // Immediately add any node with no imports and remove from this list
+      if (!stdAst.ast.imports()) {
+        orderedAsts.push(stdAst)
+        stdAsts.splice(i, 1)
+        continue
+      }
+      // For everything else, check if the dependencies are already queued up
+      const importAsts = stdAst.ast.imports()
+      let safeToAdd = true
+      for (importAst of importAsts) {
+        const depName = importAst.standardImport() ?
+          importAst.standardImport().dependency().getText().trim() :
+          importAst.fromImport().dependency().getText().trim()
+        if (!orderedAsts.some((ast) => ast.name === depName)) {
+          safeToAdd = false
+          break
+        }
+      }
+      // If it's safe, add it
+      if (safeToAdd) {
+        orderedAsts.push(stdAst)
+        stdAsts.splice(i, 1)
+        continue
+      }
+      // Otherwise, skip this one
+      i = (i + 1) % stdAsts.length
+    }
     // Now load the remainig modules based on the root scope
-    stdAsts.forEach((moduleAst) => {
+    orderedAsts.forEach((moduleAst) => {
       if (moduleAst.name != 'root.ln') {
         moduleAst.name = '@std/' + moduleAst.name.replace(/.ln$/, '')
         const stdModule = Module.populateModule(
