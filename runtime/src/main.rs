3543a5440431edd2805f12e505a0935a56d330a9
LFDP <lfdp@LFDPs-MacBook-Pro.local>
2020-05-29 13:13:10 -0500

    fix typo

--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -15,7 +15,8 @@ fn main() {
       .about("Runs compiled .agc files")
       .version(crate_version!())
       .author(crate_authors!(", "))
-      .arg_from_usage("<FILE> 'Specifies the file to load'"));
+      .arg_from_usage("<FILE> 'Specifies the file to load'"))
+    .get_matches();
   if let Some(matches) = matches.subcommand_matches("run") {
     let agc_file = matches.value_of("FILE").unwrap();
     let fp = &format!("{:}/{:}", env::current_dir().ok().unwrap().to_str().unwrap(), agc_file);
