d761a885312c4391b14b72e89e9ec50c530504c4
David Ellis <isv.damocles@gmail.com>
2020-05-29 19:37:09 -0700

    Lazily bind console.log to allow us to change it's definition at will

--- compiler/browser/runtime.js
+++ compiler/browser/runtime.js
@@ -3,7 +3,7 @@ const r = require('alan-js-runtime')
 // Redefined stdoutp and exitop to work in the browser
 module.exports = {
   ...r,
-  stdoutp: console.log,
+  stdoutp: (...args) => console.log(...args), // Lazy binding to replace `console.log` at will
   exitop: () => {
     r.emitter.removeAllListeners()
   }, // Clean up the event emitter, later we'll want a hook into the playground to show this
