a3fe397e708e3175065579ccaca8142bd9f3fd3e
David Ellis <isv.damocles@gmail.com>
2020-08-04 13:24:20 -0700

    Implement basic @std/http get function on top of curl (#158)

---
+++ bdd/spec/023_http_spec.sh
@@ -0,0 +1,33 @@
+Include build_tools.sh
+
+Describe "@std/http"
+  Describe "basic get"
+    before() {
+      sourceToAll "
+        from @std/app import start, print, exit
+        from @std/http import get
+
+        on start {
+          print(get('https://raw.githubusercontent.com/alantech/hellodep/aea1ce817a423d00107577a430a046993e4e6cad/index.ln'))
+          emit exit 0
+        }
+      "
+    }
+    BeforeAll before
+
+    after() {
+      cleanTemp
+    }
+    AfterAll after
+
+    It "runs js"
+      When run node temp.js
+      The output should eq "export const comeGetMe = \"You got me!\""
+    End
+
+    It "runs agc"
+      When run alan-runtime run temp.agc
+      The output should eq "export const comeGetMe = \"You got me!\""
+    End
+  End
+End
