bdabefc7b3b36c64fc09d7c87961c8b807a0a590
David Ellis <isv.damocles@gmail.com>
2020-09-11 12:09:44 -0700

    v0.1.7

--- .version.sh
+++ .version.sh
@@ -6,10 +6,12 @@ set -vex
 # The new version
 VERSION=$1
 
+echo Version: ${VERSION}
+
 # Update the package metadata with the specified version
-JSRUNTIME="$(jq '.version = "${VERSION}"' js-runtime/package.json)" && echo "${JSRUNTIME}" > js-runtime/package.json
-COMPILER="$(jq '.version = "${VERSION}"' compiler/package.json)" && echo "${COMPILER}" > compiler/package.json
-AVM="$(sed 's/version = .*$/version = "0.1.7"/' avm/Cargo.toml)" && echo "${AVM}" > avm/Cargo.toml
+JSRUNTIME="$(jq ".version = \"${VERSION}\"" js-runtime/package.json)" && echo "${JSRUNTIME}" > js-runtime/package.json
+COMPILER="$(jq ".version = \"${VERSION}\"" compiler/package.json)" && echo "${COMPILER}" > compiler/package.json
+AVM="$(sed "s/^version = .*$/version = \"${VERSION}\"/" avm/Cargo.toml)" && echo "${AVM}" > avm/Cargo.toml
 
 # Make sure the lockfiles are updated, too
 cd js-runtime
@@ -25,7 +27,7 @@ cargo build
 cd -
 
 # Commit and tag the update
-git add js-runtime/* compiler/* avm/*
+git add js-runtime/package.json js-runtime/yarn.lock compiler/package.json compiler/yarn.lock avm/Cargo.toml avm/Cargo.lock
 git commit -m "v${VERSION}"
 git tag v${VERSION}
 git push origin main --tags
