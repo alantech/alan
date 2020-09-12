60f4c0c4eff8be34076485cfd1b4824bf66e45d6
David Ellis <isv.damocles@gmail.com>
2020-09-11 17:03:52 -0700

    Get closure function by reference (barely) working, also make test suite use the right 'alan' binary when one is already in the PATH

--- bdd/spec/018_closure_spec.sh
+++ bdd/spec/018_closure_spec.sh
@@ -1,46 +1,82 @@
 Include build_tools.sh
 
 Describe "Closure Functions"
-  before() {
-    sourceToAll "
-      from @std/app import start, print, exit
+  Describe "closure creation and usage"
+    before() {
+      sourceToAll "
+        from @std/app import start, print, exit
 
-      fn closure(): function {
-        let num = 0
-        return fn (): int64 {
-          num = num + 1
-          return num
+        fn closure(): function {
+          let num = 0
+          return fn (): int64 {
+            num = num + 1
+            return num
+          }
         }
-      }
 
-      on start fn (): void {
-        const counter1 = closure()
-        const counter2 = closure()
-        print(counter1())
-        print(counter1())
-        print(counter2())
-        emit exit 0
-      }
-    "
-  }
-  BeforeAll before
+        on start fn (): void {
+          const counter1 = closure()
+          const counter2 = closure()
+          print(counter1())
+          print(counter1())
+          print(counter2())
+          emit exit 0
+        }
+      "
+    }
+    BeforeAll before
 
-  after() {
-    cleanTemp
-  }
-  AfterAll after
+    after() {
+      cleanTemp
+    }
+    AfterAll after
 
-  CLOSURERES="1
+    CLOSURERES="1
 2
 1"
 
-  It "runs js"
-    When run node temp.js
-    The output should eq "$CLOSURERES"
+    It "runs js"
+      When run node temp.js
+      The output should eq "$CLOSURERES"
+    End
+
+    It "runs agc"
+      When run alan run temp.agc
+      The output should eq "$CLOSURERES"
+    End
   End
 
-  It "runs agc"
-    When run alan run temp.agc
-    The output should eq "$CLOSURERES"
+  Describe "closure usage by name"
+    before() {
+      sourceToAll "
+        from @std/app import start, print, exit
+
+        fn double(x: int64): int64 = x * 2
+
+        on start {
+          const numbers = [1, 2, 3, 4, 5]
+          numbers.map(double).map(fn (x: int64): string = x.toString()).join(', ').print()
+          emit exit 0
+        }
+      "
+    }
+    BeforeAll before
+
+    after() {
+      cleanTemp
+    }
+    AfterAll after
+
+    REFOUTPUT="2, 4, 6, 8, 10"
+
+    It "runs js"
+      When run node temp.js
+      The output should eq "$REFOUTPUT"
+    End
+
+    It "runs agc"
+      When run alan run temp.agc
+      The output should eq "$REFOUTPUT"
+    End
   End
 End
