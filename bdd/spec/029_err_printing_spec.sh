f2714376b7e971735759d9a3dd1ce98aed2f5757
Alejandro Guillen <aguillenv93@gmail.com>
2020-11-02 20:07:01 +0100

    Add err printing bdd spec

---
+++ bdd/spec/029_err_printing_spec.sh
@@ -0,0 +1,59 @@
+Include build_tools.sh
+
+Describe "Error printing"
+  Describe "eprint function"
+    before() {
+      sourceToAll "
+        from @std/app import start, eprint, exit
+        on start {
+          eprint('This is an error')
+          emit exit 0
+        }
+      "
+    }
+    BeforeAll before
+
+    after() {
+      cleanTemp
+    }
+    AfterAll after
+
+    It "runs js"
+      When run node temp.js
+      The stderr should eq "This is an error"
+    End
+
+    It "runs agc"
+      When run alan run temp.agc
+      The stderr should eq "This is an error"
+    End
+  End
+
+  Describe "stderr event"
+    before() {
+      sourceToAll "
+        from @std/app import start, stderr, exit
+        on start {
+          emit stderr 'This is an error'
+          emit exit 0
+        }
+      "
+    }
+    BeforeAll before
+
+    after() {
+      cleanTemp
+    }
+    AfterAll after
+
+    It "runs js"
+      When run node temp.js
+      The stderr should eq "This is an error"
+    End
+
+    It "runs agc"
+      When run alan run temp.agc
+      The stderr should eq "This is an error"
+    End
+  End
+End
