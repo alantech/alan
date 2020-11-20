7572a8a46c37a5dcd5e256cba9af98bf632459ba
depombo <lfdepombo@gmail.com>
2020-11-20 14:42:44 -0800

    do not track avm runs on test runs and use new metric name to get clean count and stay under event limit (#324)

--- avm/src/vm/telemetry.rs
+++ avm/src/vm/telemetry.rs
@@ -15,7 +15,7 @@ pub async fn log() {
     "events": [
       {
         "user_id": "oneforallandallforone",
-        "event_type": "command",
+        "event_type": "avm-run",
         "app_version": ALAN_VERSION.unwrap(),
         "os_name": OS,
       }
