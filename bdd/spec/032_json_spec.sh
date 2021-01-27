0e59ec8c1a6962f136c3d8407340d84a83bf17ef
David Ellis <david@alantechnologies.com>
2021-01-27 08:54:30 -0800

    Adjust bdd test to avoid the interface for now

--- bdd/spec/032_json_spec.sh
+++ bdd/spec/032_json_spec.sh
@@ -5,7 +5,7 @@ Describe "JSON"
     before() {
       sourceToAll "
         from @std/app import start, print, exit
-        from @std/json import json
+        from @std/json import JSON, toJSON, toString, JSONBase, JSONNode, IsObject
 
         on start {
           1.0.toJSON().print();
