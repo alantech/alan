ace1e3a9d5090019cc9858850ae0d611f1a4beae
David Ellis <david@alantechnologies.com>
2021-01-26 16:22:43 -0800

    Basic construction and printing test

---
+++ bdd/spec/032_json_spec.sh
@@ -0,0 +1,44 @@
+Include build_tools.sh
+
+Describe "JSON"
+  Describe "basic construction and printing"
+    before() {
+      sourceToAll "
+        from @std/app import start, print, exit
+        from @std/json import json
+
+        on start {
+          1.0.toJSON().print();
+          true.toJSON().print();
+          'Hello, JSON!'.toJSON().print();
+          [1.0, 2.0, 5.0].toJSON().print();
+          toJSON().print();
+
+          emit exit 0;
+        }
+      "
+    }
+    BeforeAll before
+
+    after() {
+      cleanTemp
+    }
+    AfterAll after
+
+    BASICOUTPUT="1.0
+true
+\"Hello, JSON!\"
+[1.0, 2.0, 5.0]
+null"
+
+    It "runs js"
+      When run test_js
+      The output should eq "$BASICOUTPUT"
+    End
+
+    It "runs agc"
+      When run test_agc
+      The output should eq "$BASICOUTPUT"
+    End
+  End
+End
