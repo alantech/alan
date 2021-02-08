4e792b23065ee81d0845da8917c3ab4f33908fde
David Ellis <isv.damocles@gmail.com>
2021-02-08 09:35:29 -0800

    Get complex JSON API creation and serialization working (#400)
    * Get complex JSON API creation and serialization working
    
    * Fix pow(2, 64) bug in another spot in the root scope
    
    * Go with 2^62 as it's simpler than figuring out how to get 2^63 - 1
    
    * I think there's a regression in the implementation of resfrom in the AVM, but working around it for now
    
    * Fixing that bug in toSubtree also fixed the complex JSON test for the AVM!

--- bdd/spec/032_json_spec.sh
+++ bdd/spec/032_json_spec.sh
@@ -41,4 +41,43 @@ null"
       The output should eq "$BASICOUTPUT"
     End
   End
+
+  Describe "complex JSON type construction and printing"
+    before() {
+      sourceToAll "
+        from @std/app import start, print, exit
+        from @std/json import JSON, toString, JSONBase, JSONNode, IsObject, Null, newJSONObject, newJSONArray, addKeyVal, push
+
+        on start {
+          newJSONObject()
+            .addKeyVal('mixed', 'values')
+            .addKeyVal('work', true)
+            .addKeyVal('even', newJSONArray()
+              .push(4.0)
+              .push('arrays'))
+            .print();
+
+          emit exit 0;
+        }
+      "
+    }
+    BeforeAll before
+
+    after() {
+      cleanTemp
+    }
+    AfterAll after
+
+    COMPLEXOUTPUT="{\"mixed\": \"values\", \"work\": true, \"even\": [4, \"arrays\"]}"
+
+    It "runs js"
+      When run test_js
+      The output should eq "$COMPLEXOUTPUT"
+    End
+
+    It "runs agc"
+      When run test_agc
+      The output should eq "$COMPLEXOUTPUT"
+    End
+  End
 End
