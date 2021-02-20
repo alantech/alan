54343a5265c7b51b7ebf3c9753a872efe3c4c1be
depombo <lfdepombo@gmail.com>
2021-02-20 12:28:44 -0800

    properly get private ip address and alan version in dns vm metadata (#419)

--- avm/src/daemon/dns.rs
+++ avm/src/daemon/dns.rs
@@ -8,37 +8,55 @@ pub struct DNS {
   domain: String
 }
 
-impl DNS {
-  pub fn new(domain: &str) -> DNS {
-    // Get a new resolver with the cloudflare nameservers as the upstream recursive resolvers
-    DNS {
-      domain: domain.to_string(),
-      resolver: TokioAsyncResolver::tokio(
-        ResolverConfig::cloudflare(),
-        ResolverOpts::default(),
-      ).unwrap()
-    }
-  }
-  pub async fn get_ip_addrs(&self, app_id: &str) -> Vec<String> {
-    let name = format!("{}.{}", app_id, self.domain);
-    let err = format!("Failed to fetch TXT record with name {}", &name);
-    let resp = self.resolver.txt_lookup(name).await;
-    let mut ip_addrs = Vec::new();
-    for rec in resp.expect(&err) {
-      let data = &rec.txt_data()[0];
-      let ip_addr = DNS::ip_from_txt_data(data);
-      ip_addrs.push(ip_addr);
-    }
-    ip_addrs
-  }
+pub struct VMMetadata {
+  schema_version: String,
+  pub(crate) private_ip_addr: String,
+  pub(crate) alan_version: String,
+}
 
-  fn ip_from_txt_data(data: &[u8]) -> String {
+impl VMMetadata {
+  fn from_txt_data(data: &[u8]) -> VMMetadata {
     let txt = str::from_utf8(&*data).expect("Data in TXT record is not a valid string");
     let err = format!("VM metadata in DNS TXT record has invalid schema version: `{}`", &txt);
     let parts: Vec<&str> = txt.split("|").collect();
-    if parts.len() != 4 || parts[0] != "v1" {
+    if parts.len() != 5 || parts[0] != "v1" {
       panic!(err);
     }
-    parts[3].to_string()
+    VMMetadata {
+      schema_version: parts[0].to_string(),
+      alan_version: parts[1].to_string(),
+      private_ip_addr: parts[4].to_string(),
+    }
+  }
+}
+
+impl DNS {
+  pub fn new(domain: &str) -> DNS {
+    let mut resolver_opts = ResolverOpts::default();
+    // ignore /ect/hosts
+    resolver_opts.use_hosts_file = false;
+    // DNSSec
+    resolver_opts.validate = true;
+    // Get a new resolver with the cloudflare nameservers as the upstream recursive resolvers
+    let resolver = ResolverConfig::cloudflare_tls();
+    DNS {
+      domain: domain.to_string(),
+      resolver: TokioAsyncResolver::tokio(
+        resolver,
+        resolver_opts,
+      ).unwrap()
+    }
+  }
+  pub async fn get_vms(&self, cluster_id: &str) -> Vec<VMMetadata> {
+    let name = format!("{}.{}", cluster_id, self.domain);
+    let err = format!("Failed to fetch TXT record with name {}", &name);
+    let resp = self.resolver.txt_lookup(name).await;
+    let mut vms = Vec::new();
+    for rec in resp.expect(&err) {
+      let data = &rec.txt_data()[0];
+      let vm = VMMetadata::from_txt_data(data);
+      vms.push(vm);
+    }
+    vms
   }
 }
--- avm/src/daemon/linux.rs
+++ avm/src/daemon/linux.rs
@@ -1,13 +1,13 @@
-use anycloud::deploy::post_v1;
+use anycloud::deploy;
 use serde::Serialize;
 use serde_json::{json, Value};
 use futures::stream::StreamExt;
 use heim::{
-  host,
   cpu::{self, os::linux::CpuTimeExt},
   memory::{self, os::linux::MemoryExt},
   units::{information::kilobyte, time::second}
 };
+use tokio::process::Command;
 use tokio::task;
 use tokio::time::{Duration, sleep};
 
@@ -39,70 +39,102 @@ struct VMStatsV1 {
   freeSwapKb: u64,
 }
 
-pub async fn post(endpoint: &str, body: Value) -> String {
-  let resp = post_v1(endpoint, body).await;
+const DOMAIN: &'static str = "anycloudapp.com";
+
+async fn get_private_ip() -> String {
+  let res = Command::new("hostname")
+    .arg("-I")
+    .output()
+    .await;
+  let err = "Failed to execute `hostname`";
+  let stdout = res.expect(err).stdout;
+  String::from_utf8(stdout)
+    .expect(err)
+    .trim()
+    .split_whitespace()
+    .next()
+    .unwrap()
+    .to_string()
+}
+
+async fn post_v1(endpoint: &str, body: Value) -> String {
+  let resp = deploy::post_v1(endpoint, body).await;
   match resp {
     Ok(res) => res,
     Err(err) => err.to_string(),
   }
 }
 
-pub async fn start(app_id: &str, agz_b64: &str, deploy_token: &str) {
-  let app_id = app_id.to_string();
+async fn post_v1_scale(cluster_id: &str, agz_b64: &str, deploy_token: &str, alan_version: &str, delta: i32) -> String {
+  let scale_body = json!({
+    "alanVersion": alan_version,
+    "clusterId": cluster_id,
+    "agzB64": agz_b64,
+    "deployToken": deploy_token,
+    "clusterDelta": delta,
+  });
+  post_v1("scale", scale_body).await
+}
+
+async fn get_v1_stats() -> VMStatsV1 {
+  let memory = memory::memory().await.expect("Failed to get system memory information");
+  let swap = memory::swap().await.expect("Failed to get swap information");
+  VMStatsV1 {
+    cpuSecs: cpu::times().map(|r| {
+      let cpu = r.expect("Failed to get CPU times");
+      CPUSecsV1 {
+        user: cpu.user().get::<second>(),
+        system: cpu.system().get::<second>(),
+        idle: cpu.idle().get::<second>(),
+        interrupt: cpu.irq().get::<second>(),
+        nice: cpu.nice().get::<second>(),
+        iowait: cpu.io_wait().get::<second>(),
+      }
+    }).collect().await,
+    totalMemoryKb: memory.total().get::<kilobyte>(),
+    availableMemoryKb: memory.available().get::<kilobyte>(),
+    freeMemoryKb: memory.free().get::<kilobyte>(),
+    activeMemoryKb: memory.active().get::<kilobyte>(),
+    usedMemoryKb: memory.used().get::<kilobyte>(),
+    totalSwapKb: swap.total().get::<kilobyte>(),
+    usedSwapKb: swap.used().get::<kilobyte>(),
+    freeSwapKb: swap.free().get::<kilobyte>(),
+  }
+}
+
+// returns cluster delta
+async fn post_v1_stats(deploy_token: &str, cluster_size: usize) -> i32 {
+  let vm_stats = get_v1_stats().await;
+  let stats_body = json!({
+    "deployToken": deploy_token,
+    "vmStats": vm_stats,
+    "clusterSize": cluster_size,
+  });
+  let resp = post_v1("stats", stats_body).await;
+  resp.parse().expect("Failed to parse cluster delta")
+}
+
+pub async fn start(cluster_id: &str, agz_b64: &str, deploy_token: &str) {
+  let cluster_id = cluster_id.to_string();
   let deploy_token = deploy_token.to_string();
   let agzb64 = agz_b64.to_string();
   task::spawn(async move {
-    let dns = DNS::new("alandeploy.com");
-    let ip_addrs = dns.get_ip_addrs(&app_id).await;
-    let cluster_size = ip_addrs.len();
-    let lrh = LogRendezvousHash::new(ip_addrs);
+    let dns = DNS::new(DOMAIN);
+    let vms = dns.get_vms(&cluster_id).await;
+    let cluster_size = vms.len();
+    let ips = vms.iter().map(|vm| vm.private_ip_addr.to_string()).collect();
+    let alan_version = &vms[0].alan_version;
+    let lrh = LogRendezvousHash::new(ips);
     let leader_ip = lrh.get_leader_id();
-    let platform = host::platform().await.expect("Failed to get platform information");
-    let self_ip = platform.hostname().replace("ip-", "");
+    let self_ip = get_private_ip().await;
     if leader_ip == self_ip {
       // TODO better period determination
       let period = Duration::from_secs(30);
       loop {
-        let memory = memory::memory().await.expect("Failed to get system memory information");
-        let swap = memory::swap().await.expect("Failed to get swap information");
-        let stats = VMStatsV1 {
-          cpuSecs: cpu::times().map(|r| {
-            let cpu = r.expect("Failed to get CPU times");
-            CPUSecsV1 {
-              user: cpu.user().get::<second>(),
-              system: cpu.system().get::<second>(),
-              idle: cpu.idle().get::<second>(),
-              interrupt: cpu.irq().get::<second>(),
-              nice: cpu.nice().get::<second>(),
-              iowait: cpu.io_wait().get::<second>(),
-            }
-          }).collect().await,
-          totalMemoryKb: memory.total().get::<kilobyte>(),
-          availableMemoryKb: memory.available().get::<kilobyte>(),
-          freeMemoryKb: memory.free().get::<kilobyte>(),
-          activeMemoryKb: memory.active().get::<kilobyte>(),
-          usedMemoryKb: memory.used().get::<kilobyte>(),
-          totalSwapKb: swap.total().get::<kilobyte>(),
-          usedSwapKb: swap.used().get::<kilobyte>(),
-          freeSwapKb: swap.free().get::<kilobyte>(),
-        };
-        
-        let stats_body = json!({
-          "deployToken": deploy_token,
-          "vmStats": stats,
-          "clusterSize": cluster_size,
-        });
-    
-        let resp = post("stats", stats_body).await;
-        let delta: i32 = resp.parse().expect("Failed to parse cluster delta");
+        let delta = post_v1_stats(&deploy_token, cluster_size).await;
+        println!("VM stats sent for cluster {} of size {}. Cluster delta: {}.", cluster_id, cluster_size, delta);
         if delta != 0 {
-          let scale_body = json!({
-            "appId": app_id,
-            "agzB64": agzb64,
-            "deployToken": deploy_token,
-            "clusterDelta": delta,
-          });
-          post("scale", scale_body).await;
+          post_v1_scale(&cluster_id, &agzb64, &deploy_token, alan_version, delta).await;
         }
         sleep(period).await
       }
