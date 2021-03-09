3968f767bb5b8086d54a38241256067fd09ae478
David Ellis <david@alantechnologies.com>
2021-03-08 20:12:05 -0800

    Get it compiling on non-linux again

--- avm/src/daemon/nonlinux.rs
+++ avm/src/daemon/nonlinux.rs
@@ -1,3 +1,10 @@
-pub async fn start(_app_id: &str, _agz_b64: &str, _deploy_token: &str, _domain: &str) {
+pub async fn start(
+  _app_id: &str,
+  _agz_b64: &str,
+  _deploy_token: &str,
+  _domain: &str,
+  _priv_key_b64: Option<&str>,
+  _cert_b64: Option<&str>,
+) {
   panic!("Daemon only works on Linux");
 }
