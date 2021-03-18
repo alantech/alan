5c87161dd2c5f77e94111bcc8f52ebb6306342b0
David Ellis <isv.damocles@gmail.com>
2021-03-18 12:56:24 -0700

    Refactor the daemon-related code to be more self-contained in the daemon module, and add a secret string for use in guarding access to the control port (#458)
    * Refactor the daemon-related code to be more self-contained in the daemon module, and add a secret string for use in guarding access to the control port
    
    * Fix formatting
    
    * Include the secret in the stats body passed back to the 'deploy' service and make changes suggested by @cdmistman
    
    * Fix lint
    
    * Remove outdated comment and useless 'pub'
    
    * secret_string -> cluster_secret
    
    * Missed one

--- avm/src/main.rs
+++ avm/src/main.rs
@@ -9,7 +9,7 @@ use serde_json::json;
 use tokio::runtime::Builder;
 
 use crate::compile::compile::compile;
-use crate::daemon::daemon::start;
+use crate::daemon::daemon::{start, CLUSTER_SECRET};
 use crate::vm::run::run_file;
 use crate::vm::telemetry;
 
@@ -81,6 +81,7 @@ fn main() {
       .arg_from_usage("<DOMAIN> 'Specifies the application domain'")
       .arg_from_usage("-k, --private-key=[PRIV_KEY_B64] 'An optional base64 encoded private key for HTTPS mode'")
       .arg_from_usage("-c, --certificate=[CERT_B64] 'An optional base64 encoded certificate for HTTPS mode'")
+      .arg_from_usage("-s, --cluster-secret=[CLUSTER_SECRET] 'An optional secret string to constrain access to the control port'")
     )
     .arg_from_usage("[SOURCE] 'Specifies a source ln file to compile and run'");
 
@@ -169,6 +170,10 @@ fn main() {
         let domain = matches.value_of("DOMAIN").unwrap();
         let priv_key_b64 = matches.value_of("private-key");
         let cert_b64 = matches.value_of("certificate");
+        let cluster_secret = matches.value_of("cluster-secret");
+        CLUSTER_SECRET
+          .set(cluster_secret.map(|s| s.to_string()))
+          .unwrap();
         start(
           app_id,
           agz_b64,
