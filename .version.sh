8c4c8319714f70b3e3f9c114c88e0a0cb8f8a5c3
depombo <lfdepombo@gmail.com>
2021-04-14 13:00:47 -0700

    use alan compiler in repo to build anycloud cli (#487)

--- .version.sh
+++ .version.sh
@@ -12,6 +12,7 @@ echo Version: ${VERSION}
 JSRUNTIME="$(jq ".version = \"${VERSION}\"" js-runtime/package.json)" && echo "${JSRUNTIME}" > js-runtime/package.json
 COMPILER="$(jq ".version = \"${VERSION}\"" compiler/package.json)" && echo "${COMPILER}" > compiler/package.json
 AVM="$(sed "s/^version = .*$/version = \"${VERSION}\"/" avm/Cargo.toml)" && echo "${AVM}" > avm/Cargo.toml
+ANYCLOUD="$(sed "s/^version = .*$/version = \"${VERSION}\"/" anycloud/cli/Cargo.toml)" && echo "${ANYCLOUD}" > anycloud/cli/Cargo.toml
 
 # Make sure the lockfiles are updated, too
 cd js-runtime
@@ -22,12 +23,16 @@ cd compiler
 yarn
 cd -
 
+cd anycloud/cli
+cargo build
+cd -
+
 cd avm
 cargo build
 cd -
 
 # Commit and tag the update
-git add js-runtime/package.json js-runtime/yarn.lock compiler/package.json compiler/yarn.lock avm/Cargo.toml avm/Cargo.lock
+git add js-runtime/package.json js-runtime/yarn.lock compiler/package.json compiler/yarn.lock avm/Cargo.toml avm/Cargo.lock anycloud/cli/Cargo.toml anycloud/cli/Cargo.lock
 git commit -m "v${VERSION}"
 git tag v${VERSION}
 git push origin main --tags
