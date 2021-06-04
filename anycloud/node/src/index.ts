a5ad5e6100f77a8d0c012e3500cac9c1a2e93ec3
Alejandro Guillen <aguillenv93@gmail.com>
2021-06-04 17:01:35 +0200

    Export new datastore proxy

--- anycloud/node/src/index.ts
+++ anycloud/node/src/index.ts
@@ -1,6 +1,7 @@
-import { DS } from './datastore';
+import { datastore } from './datastore';
 
 export const PORT = 8088; // http://img2.wikia.nocookie.net/__cb20080604102014/starwars/images/3/3b/8t88.png
+export const DS = datastore;
 
 export default {
   PORT,
