1ed136f1d9d179cfbb304a877aa790d87c86c5c4
Colton Donnelly <colton@donn.io>
2021-06-17 19:01:56 +0000

    add unimported types bdd test

--- bdd/spec/016_type_spec.sh
+++ bdd/spec/016_type_spec.sh
@@ -1,6 +1,6 @@
 Include build_tools.sh
 
-Describe "Type detection"
+Describe "Types"
   Describe "user types and generics"
     before() {
       sourceToAll "
@@ -57,6 +57,49 @@ Describe "Type detection"
     End
 
     It "runs agc"
+      When run test_agc
+      The output should eq "$GENTYPEOUTPUT"
+    End
+  End
+
+  Describe "using non-imported type returned by imported function"
+    before() {
+      sourceToTemp "
+        from @std/app import start, exit
+        from @std/http import fetch, Request
+
+        on start {
+          arghFn('{\"test\":\"test\"}');
+          emit exit 0;
+        }
+
+        fn arghFn(arghStr: string) {
+          fetch(new Request {
+              method: 'POST',
+              url: 'https://reqbin.com/echo/post/json',
+              headers: newHashMap('Content-Length', arghStr.length().toString()),
+              body: arghStr,
+            });
+        }
+      "
+    }
+    BeforeAll before
+
+    after() {
+      cleanTemp
+    }
+    AfterAll after
+
+    OUTPUT="{\"success\":\"true\"}"
+
+    It "runs js"
+      Pending unimported-types-returned-by-imported-functions
+      When run test_js
+      The output should eq "$GENTYPEOUTPUT"
+    End
+
+    It "runs agc"
+      Pending unimported-types-returned-by-imported-functions
       When run test_agc
       The output should eq "$GENTYPEOUTPUT"
     End
