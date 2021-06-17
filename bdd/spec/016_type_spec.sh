9fbd7fc3237913ebc754247f882abe2949ac088c
Colton Donnelly <colton@donn.io>
2021-06-17 17:42:38 -0400

    bdd test for bugs (#575)
    * bdd test for bug with arguments in closures that @aguillenv found
    
    * add unimported types bdd test
    
    * the unimported test should have no output
    
    * use node server to test the output
    
    * do not launch the actual alan programs in the non-imported type test

--- bdd/spec/016_type_spec.sh
+++ bdd/spec/016_type_spec.sh
@@ -1,6 +1,6 @@
 Include build_tools.sh
 
-Describe "Type detection"
+Describe "Types"
   Describe "user types and generics"
     before() {
       sourceToAll "
@@ -59,6 +59,74 @@ Describe "Type detection"
     It "runs agc"
       When run test_agc
       The output should eq "$GENTYPEOUTPUT"
+    End
+  End
+
+  Describe "using non-imported type returned by imported function"
+    before() {
+      sourceToTemp "
+        from @std/app import start, exit
+        from @std/http import fetch, Request
+
+        on start {
+          arghFn('{\"test\":\"test\"}');
+          emit exit 0;
+        }
+
+        fn arghFn(arghStr: string) {
+          fetch(new Request {
+              method: 'POST',
+              url: 'https://reqbin.com/echo/post/json',
+              headers: newHashMap('Content-Length', arghStr.length().toString()),
+              body: arghStr,
+            });
+        }
+      "
+      sourceToFile test_server.js "
+        const http = require('http');
+
+        http.createServer((req, res) => {
+          console.log('received');
+          res.end('Hello, world!');
+        }).listen(8088);
+      "
+    }
+    BeforeAll before
+
+    after() {
+      cleanTemp
+    }
+    AfterAll after
+
+    afterEach() {
+      kill $PID1
+      wait $PID1 2>/dev/null
+      # kill $PID2
+      # wait $PID2 2>/dev/null
+      return 0
+    }
+    After afterEach
+
+    It "runs js"
+      Pending unimported-types-returned-by-imported-functions
+      node test_$$/test_server.js 1>test_$$/test_server.js.out 2>/dev/null &
+      PID1=$!
+      # node test_$$/temp.js 1>/dev/null &
+      # PID2=$!
+      sleep 1
+      When run cat test_$$/test_server.js.out
+      The output should eq "received"
+    End
+
+    It "runs agc"
+      Pending unimported-types-returned-by-imported-functions
+      node test_$$/test_server.js 1>test_$$/test_server.agc.out 2>/dev/null &
+      PID1=$!
+      # alan run test_$$/temp.agc 1>/dev/null 2>/dev/null  &
+      # PID2=$!
+      sleep 1
+      When run cat test_$$/test_server.agc.out
+      The output should eq "received"
     End
   End
 End
