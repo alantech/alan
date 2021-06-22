f555e63e9be1ed1b457c89573157b7ff77ecd70a
Colton Donnelly <colton@donn.io>
2021-06-22 16:31:16 +0000

    add style stuff

---
+++ compiler/.eslintrc.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
