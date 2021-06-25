5707f8e6e176f729c79fdb363fbd53ffe7157013
Alejandro Guillen <aguillenv93@gmail.com>
2021-06-25 16:16:02 +0200

    Refactor anycloud common methods with tmp dir

--- anycloud/cli/src/common/mod.rs
+++ anycloud/cli/src/common/mod.rs
@@ -2,22 +2,40 @@ use std::env;
 use std::fs::read;
 use std::process::Command;
 
-async fn get_file(file_path: &str) -> Result<Vec<u8>, String> {
-  let pwd = env::current_dir();
-  match pwd {
-    Ok(pwd) => match read(format!("{}/{}", pwd.display(), file_path)) {
-      Ok(file) => Ok(file),
-      Err(_) => Err(format!("No Dockerfile at {}", pwd.display()).into()),
-    },
-    Err(_) => {
-      warn!(InvalidPwd, "Current working directory value is invalid");
-      std::process::exit(1);
+use tempdir::TempDir;
+
+async fn get_file(file_name: &str, file_path: Option<&str>) -> Result<Vec<u8>, String> {
+  match file_path {
+    Some(file_path) => {
+      println!("{}", format!("{}/{}", file_path, file_name));
+      let output = Command::new("ls")
+        .arg(format!("{}/{}", file_path, file_name))
+        .output()
+        .unwrap();
+      println!("{:?}", output);
+      match read(format!("{}/{}", file_path, file_name)) {
+        Ok(file) => Ok(file),
+        Err(_) => Err(format!("No {} at {}", file_name, file_path).into()),
+      }
+    }
+    None => {
+      let pwd = env::current_dir();
+      match pwd {
+        Ok(pwd) => match read(format!("{}/{}", pwd.display(), file_name)) {
+          Ok(file) => Ok(file),
+          Err(_) => Err(format!("No {} at {}", file_name, pwd.display()).into()),
+        },
+        Err(_) => {
+          warn!(InvalidPwd, "Current working directory value is invalid");
+          std::process::exit(1);
+        }
+      }
     }
   }
 }
 
 pub async fn get_dockerfile_b64() -> String {
-  match get_file("Dockerfile").await {
+  match get_file("Dockerfile", None).await {
     Ok(file) => base64::encode(file),
     Err(err) => {
       warn!(NoDockerFile, "{}", err);
@@ -27,7 +45,7 @@ pub async fn get_dockerfile_b64() -> String {
 }
 
 pub async fn get_env_file_b64(env_file_path: String) -> String {
-  match get_file(&env_file_path).await {
+  match get_file(&env_file_path, None).await {
     Ok(file) => base64::encode(file),
     Err(err) => {
       warn!(NoEnvFile, "{}", err);
@@ -37,7 +55,7 @@ pub async fn get_env_file_b64(env_file_path: String) -> String {
 }
 
 pub async fn get_agz_file_b64(agz_file_path: String) -> String {
-  match get_file(&agz_file_path).await {
+  match get_file(&agz_file_path, None).await {
     Ok(file) => base64::encode(file),
     Err(err) => {
       warn!(NoDaemonAGZFile, "{}", err);
@@ -46,19 +64,33 @@ pub async fn get_agz_file_b64(agz_file_path: String) -> String {
   }
 }
 
-pub async fn get_app_tar_gz_b64(should_remove_app_tar: bool) -> String {
+pub async fn get_app_tar_gz_b64(is_temporal: bool) -> String {
   git_status().await;
-  git_archive_app_tar().await;
-  let app_tar_gz = match get_file("app.tar.gz").await {
+  let file_name = "app.tar.gz";
+  let tmp_dir = TempDir::new("anycloud");
+  let mut tmp_dir_path: Option<&str> = None;
+  if is_temporal {
+    tmp_dir_path = match &tmp_dir {
+      Ok(dir) => dir.path().to_str(),
+      Err(e) => {
+        warn!(NoTmpDir, "Error creating temporal directory. {}", e);
+        std::process::exit(1);
+      }
+    }
+  }
+
+  let file_path = match tmp_dir_path {
+    Some(tmp_dir) => format!("{}/{}", tmp_dir, file_name),
+    None => file_name.to_string()
+  };
+  git_archive_app_tar(&file_path).await;
+  let app_tar_gz = match get_file(file_name, tmp_dir_path).await {
     Ok(file) => file,
     Err(err) => {
       warn!(NoTarballFile, "{}", err);
       std::process::exit(1);
     }
   };
-  if should_remove_app_tar {
-    git_remove_app_tar().await;
-  }
   return base64::encode(app_tar_gz);
 }
 
@@ -79,30 +111,17 @@ async fn git_status() {
   }
 }
 
-async fn git_archive_app_tar() {
+async fn git_archive_app_tar(file_path: &str) {
   let output = Command::new("git")
     .arg("archive")
     .arg("--format=tar.gz")
     .arg("-o")
-    .arg("app.tar.gz")
+    .arg(file_path)
     .arg("HEAD")
     .output()
     .unwrap();
-
   if output.status.code().unwrap() != 0 {
     warn!(NoGit, "Your code must be managed by git in order to deploy correctly, please run `git init && git commit -am \"Initial commit\"` and try again.");
-    std::process::exit(output.status.code().unwrap());
-  }
-}
-
-async fn git_remove_app_tar() {
-  let output = Command::new("rm").arg("app.tar.gz").output().unwrap();
-
-  if output.status.code().unwrap() != 0 {
-    warn!(
-      DeleteTmpAppTar,
-      "Somehow could not delete temporary app.tar.gz file"
-    );
     std::process::exit(output.status.code().unwrap());
   }
 }
