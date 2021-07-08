ff2bbd04cbbea745cb83f621b2dfcfa0baf182e9
Alejandro Guillen <aguillenv93@gmail.com>
2021-07-08 18:29:19 +0200

    Refactor stats endpoint to ack

--- avm/src/daemon/daemon.rs
+++ avm/src/daemon/daemon.rs
@@ -123,27 +123,25 @@ async fn post_v1_scale(
   }
 }
 
-// returns cluster delta
-async fn post_v1_stats(
-  vm_stats: Vec<VMStatsV1>,
+// acknowledge deploy service to refresh secret
+async fn post_v1_ack(
   cluster_id: &str,
   deploy_token: &str,
 ) -> DaemonResult<String> {
-  let mut stats_body = json!({
+  let mut ack_body = json!({
     "deployToken": deploy_token,
-    "vmStats": vm_stats,
     "clusterId": cluster_id,
   });
   let cluster_secret = CLUSTER_SECRET.get().unwrap();
   if let Some(cluster_secret) = cluster_secret.as_ref() {
-    stats_body
+    ack_body
       .as_object_mut()
       .unwrap()
       .insert("clusterSecret".to_string(), json!(cluster_secret));
   } else {
     error!(NoClusterSecret, "No cluster secret found.").await;
   }
-  Ok(post_v1("stats", stats_body).await)
+  Ok(post_v1("ack", ack_body).await)
 }
 
 async fn run_agz_b64(agz_b64: &str) -> DaemonResult<()> {
@@ -331,17 +329,17 @@ pub async fn start(is_local_anycloud_app: bool, local_agz_b64: Option<String>) {
             }
             if stats.len() >= 4 {
               let mut factor = String::from("1");
-              let stats_factor = post_v1_stats(stats.to_owned(), &cluster_id, &deploy_token).await;
+              post_v1_ack(&cluster_id, &deploy_token).await;
               stats = Vec::new();
-              if let Ok(stats_factor) = stats_factor {
-                factor = stats_factor;
-              } else if let Err(err) = stats_factor {
-                error!(
-                  PostFailed,
-                  "Failed sending stats for cluster {}: {}", &cluster_id, err
-                )
-                .await;
-              }
+              // if let Ok(stats_factor) = stats_factor {
+              //   factor = stats_factor;
+              // } else if let Err(err) = stats_factor {
+              //   error!(
+              //     PostFailed,
+              //     "Failed sending stats for cluster {}: {}", &cluster_id, err
+              //   )
+              //   .await;
+              // }
               println!(
                 "VM stats sent for cluster {} of size {}. Cluster factor: {}.",
                 cluster_id, cluster_size, factor
