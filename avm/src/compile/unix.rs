e9ec8cdcc98ce0af94a5256b393734623aa977d7
Colton Donnelly <colton@donn.io>
2021-08-03 11:14:39 -0400

    make the change for windows, hope it works

--- avm/src/compile/unix.rs
+++ avm/src/compile/unix.rs
@@ -11,8 +11,7 @@ use tempdir::TempDir;
 pub fn compile(source_file: &str, dest_file: &str, silent: bool) -> i32 {
   let compiler = include_bytes!("../../../compiler/alan-compile");
   let tempdir = TempDir::new(id().to_string().as_str()).unwrap();
-  let mut alan_compile_path = tempdir.path().to_owned();
-  alan_compile_path.push("alan-compile");
+  let alan_compile_path = tempdir.path().join("alan-compile");
   let mut f = File::create(&alan_compile_path).unwrap();
   f.write_all(compiler).unwrap();
   let metadata = f.metadata().unwrap();
@@ -21,16 +20,16 @@ pub fn compile(source_file: &str, dest_file: &str, silent: bool) -> i32 {
   f.set_permissions(permissions).unwrap();
   drop(f);
   let mut source_path = env::current_dir().unwrap();
-  source_path = source_path.join(source_file);
+  source_path.push(source_file);
   let mut dest_path = env::current_dir().unwrap();
-  dest_path = dest_path.join(dest_file);
+  dest_path.push(dest_file);
   let output = Command::new("sh")
     .arg("-c")
     .arg(format!(
       "{} {} {}",
-      alan_compile_path.as_path().display(),
-      source_path.as_path().display(),
-      dest_path.as_path().display(),
+      alan_compile_path.display(),
+      source_path.display(),
+      dest_path.display(),
     ))
     .output()
     .unwrap();
--- avm/src/compile/windows.rs
+++ avm/src/compile/windows.rs
@@ -5,36 +5,29 @@ use std::io::prelude::*;
 use std::process::{id, Command};
 use std::str;
 
+use tempdir::TempDir;
+
 pub fn compile(source_file: &str, dest_file: &str, silent: bool) -> i32 {
   let compiler = include_bytes!("..\\..\\..\\compiler\\alan-compile.exe");
-  let mut dir = env::temp_dir();
-  dir.push(id().to_string());
-  let dir0 = dir.clone();
-  let dir1 = dir.clone();
-  create_dir(dir0.into_os_string()).unwrap();
-  dir.push("alan-compile.exe");
-  let dir2 = dir.clone(); // Bleh
-  let dir3 = dir.clone(); // Bleh
-  let dir4 = dir.clone(); // Bleh
-  let mut f = File::create(dir).unwrap();
+  let tempdir = TempDir::new(id().to_string().as_str()).unwrap();
+  let alan_compile_path = tempdir.path().join("alan-compile.exe");
+  let mut f = File::create(&alan_compile_path).unwrap();
   f.write_all(compiler).unwrap();
   drop(f);
   let mut source_path = env::current_dir().unwrap();
-  source_path.push(source_file);
+  source_path = source_path.join(source_file).unwrap();
   let mut dest_path = env::current_dir().unwrap();
-  dest_path.push(dest_file);
+  dest_path = dest_path.join(dest_file).unwrap();
   let output = Command::new("cmd")
     .arg("/C")
     .arg(format!(
       "{} {} {}",
-      &dir2.into_os_string().into_string().unwrap(),
-      source_path.into_os_string().into_string().unwrap(),
-      dest_path.into_os_string().into_string().unwrap(),
+      alan_compile_path.display(),
+      source_path.display(),
+      dest_path.display(),
     ))
     .output()
     .unwrap();
-  remove_file(dir3.as_path()).unwrap();
-  remove_dir(dir1.as_path()).unwrap();
   if output.stdout.len() > 0 && !silent {
     print!("{}", str::from_utf8(&output.stdout).unwrap());
   }
