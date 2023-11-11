9ff31ddb254eaba700def0f59042e9ecd370c49c
David Ellis <isv.damocles@gmail.com>
2023-11-10 20:14:33 -0600

    Fix formatting

--- compiler/.eslintrc.js
+++ compiler/.eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
