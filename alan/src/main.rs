5b1db0481bcd2e932b3c1216477f9fcac69852c6
David Ellis <isv.damocles@gmail.com>
2024-01-25 12:26:53 -0600

    Initial commit for the 0.2.0 dev branch

---
+++ alan/src/main.rs
@@ -0,0 +1,30 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, propagate_version=true)]
+struct Cli {
+  #[command(subcommand)]
+  commands: Option<Commands>,
+
+  #[arg(value_name="LN_FILE", help=".ln source file to interpret")]
+  file: Option<String>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+  #[command(about="Compile .ln file(s) to an executable")]
+  Compile {
+    #[arg(value_name="LN_FILE", help=".ln source file to compile. ./index.ln if not specified", default_value="./index.ln")]
+    file: String,
+  },
+  #[command(about="Install dependencies for your Alan project")]
+  Install {
+    #[arg(value_name="DEP_FILE", help="The .ln install script to run and install the necessary dependencies into /dependences. ./.dependencies.ln if not specified", default_value="./.dependencies.ln")]
+    file: String,
+  },
+}
+
+fn main() {
+    let args = Cli::parse();
+    println!("{:?}", args);
+}
