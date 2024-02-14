056aae93374c760f3b64e4042ae74b9188cc20e9
David Ellis <isv.damocles@gmail.com>
2024-02-14 10:13:03 -0600

    Fail fast if 'rustc' is not available

--- alan/src/compile.rs
+++ alan/src/compile.rs
@@ -6,7 +6,11 @@ use std::process::{Command, Stdio};
 use crate::lntors::lntors;
 
 pub fn compile(source_file: String) -> Result<(), Box<dyn std::error::Error>> {
+    // Fail if rustc is not present
+    Command::new("which").arg("rustc").output()?;
+    // Generate the rust code to compile
     let rs_str = lntors(source_file.clone())?;
+    // Shove it into a temp file for rustc
     let tmp_file = match PathBuf::from(source_file).file_stem() {
         Some(pb) => format!("{}.rs", pb.to_string_lossy().to_string()),
         None => {
@@ -14,11 +18,13 @@ pub fn compile(source_file: String) -> Result<(), Box<dyn std::error::Error>> {
         }
     };
     write(&tmp_file, rs_str)?;
+    // Build the executable
     Command::new("rustc")
         .arg(&tmp_file)
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .output()?;
+    // Drop the temp file
     remove_file(tmp_file)?;
     Ok(())
 }
