9cd87a176320dbc13b5dc44889537cfa6a693d27
David Ellis <isv.damocles@gmail.com>
2024-02-15 23:36:25 -0600

    copy the import tests for now

--- src/compile.rs
+++ src/compile.rs
@@ -3674,3 +3674,54 @@ test!(cmd_sequential => r#"
     }"#;
     stdout "foobar\n";
 );
+
+/* TODO: Module import testing once the test macros are improved
+
+Describe "Module imports"
+  Describe "can import with trailing whitespace"
+    before() {
+      sourceToFile piece.ln "
+        export type Piece {
+          owner: bool,
+        }
+      "
+      sourceToAll "
+        from @std/app import start, print, exit
+        // Intentionally put an extra space after the import
+        from ./piece import Piece 
+
+        on start {
+          const piece = new Piece {
+            owner: false,
+          };
+          print('Hello, World!');
+          if piece.owner == true {
+            print('OK');
+          } else {
+            print('False');
+          }
+          emit exit 0;
+        }
+      "
+    }
+    BeforeAll before
+
+    after() {
+      cleanTemp
+    }
+    AfterAll after
+
+    It "runs js"
+      When run test_js
+      The output should eq "Hello, World!
+False"
+    End
+
+    It "runs agc"
+      When run test_agc
+      The output should eq "Hello, World!
+False"
+    End
+  End
+End
+*/
