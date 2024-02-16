debbdcd33db12bcceeeea44c9f0aed2c91c6ed2a
David Ellis <isv.damocles@gmail.com>
2024-02-15 19:42:44 -0600

    Macro-ify the compile test code, too

--- src/compile.rs
+++ src/compile.rs
@@ -5,6 +5,8 @@ use std::process::{Command, Stdio};
 
 use crate::lntors::lntors;
 
+/// The `compile` function is a very thin wrapper on top of `lntors`, just handling the file
+/// loading and temporary file storage and removal on the path to generating the binary.
 pub fn compile(source_file: String) -> Result<(), Box<dyn std::error::Error>> {
     // Fail if rustc is not present
     Command::new("which").arg("rustc").output()?;
@@ -29,24 +31,65 @@ pub fn compile(source_file: String) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-#[cfg(test)]
-mod test_compile {
-    #[test]
-    fn hello_world() -> Result<(), Box<dyn std::error::Error>> {
-        // Simple Hello, World app test
-        // TODO: Switch to proposed fully-qualified function export instead of event-based start
-        super::write("./hello.ln", r#"
-            on start {
-                print("Hello, World!");
+/// The majority of this file is dedicated to a comprehensive test suite, converted from the prior
+/// test suite using macros to make it a bit more dense than it would have been otherwise.
+/// The macro here is composed of three parts: The test program source, the expected exit code
+/// (usually 0 so it is optional), and the expected stdout or stderr text (also optional).
+/// The syntax to use it is something like:
+/// test!(hello_world => r#"
+///     on start {
+///         print("Hello, World!");
+///     }
+///     "#;
+///     stdout "Hello, World!\n";
+///     status 0;
+/// );
+macro_rules! test {
+    ( $rule: ident => $code:expr; $( $type:ident $test_val:expr);+ $(;)? ) => {
+        #[cfg(test)]
+        mod $rule {
+            #[test]
+            fn $rule() -> Result<(), Box<dyn std::error::Error>> {
+                let filename = format!("{}.ln", stringify!($rule));
+                super::write(&filename, $code)?;
+                assert_eq!((), super::compile(filename.to_string())?);
+                let run = super::Command::new(format!("./{}", stringify!($rule))).output()?;
+                $( $type!($test_val, &run); )+
+                // Cleanup the temp files. TODO: Make this happen regardless of test failure?
+                super::remove_file(&filename)?;
+                super::remove_file(stringify!($rule))?;
+                Ok(())
             }
-        "#)?;
-        assert_eq!((), super::compile("./hello.ln".to_string())?);
-        // Confirm the generated binary does what it should
-        let hello_world = String::from_utf8(super::Command::new("./hello").output()?.stdout)?;
-        // Drop the generated files first just in case
-        super::remove_file("./hello.ln")?;
-        super::remove_file("./hello")?;
-        assert_eq!("Hello, World!\n", &hello_world);
-        Ok(())
+        }
     }
 }
+#[cfg(test)]
+macro_rules! stdout {
+    ( $test_val:expr, $real_val:expr ) => {
+        let std_out = String::from_utf8($real_val.stdout.clone())?;
+        assert_eq!($test_val, &std_out);
+    }
+}
+#[cfg(test)]
+macro_rules! stderr {
+    ( $test_val:expr, $real_val:expr ) => {
+        let std_err = String::from_utf8($real_val.stderr.clone())?;
+        assert_eq!($test_val, &std_err);
+    }
+}
+#[cfg(test)]
+macro_rules! status {
+    ( $test_val:expr, $real_val:expr ) => {
+        let status = $real_val.status.code().unwrap();
+        assert_eq!($test_val, status);
+    }
+}
+
+// The only test that works for now
+test!(hello_world => r#"
+    on start {
+        print("Hello, World!");
+    }"#;
+    stdout "Hello, World!\n";
+    status 0;
+);
