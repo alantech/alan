b88ef68965ad29d1c0115306e68ec43a854f6e4a
David Ellis <isv.damocles@gmail.com>
2024-02-15 23:31:47 -0600

    Convert the tree tests

--- src/compile.rs
+++ src/compile.rs
@@ -3520,3 +3520,108 @@ test!(seq_recurse_decrement_regression_test => r#"
     }"#;
     stdout "1 3 6\n"; // TODO: Same concern, do regression tests matter for a different codebase?
 );
+
+// Tree
+
+test!(tree_construction_and_access => r#"
+    from @std/app import start, print, exit
+
+    on start {
+      const myTree = newTree('foo');
+      const barNode = myTree.addChild('bar');
+      const bazNode = myTree.addChild('baz');
+      const bayNode = barNode.addChild('bay');
+
+      print(myTree.getRootNode() || 'wrong');
+      print(bayNode.getParent() || 'wrong');
+      print(myTree.getChildren().map(fn (c: Node<string>): string = c || 'wrong').join(', '));
+
+      emit exit 0;
+    }"#;
+    stdout "foo\nbar\nbar, baz\n";
+);
+test!(tree_user_defined_types => r#"
+    from @std/app import start, print, exit
+
+    type Foo {
+      foo: string,
+      bar: bool,
+    }
+
+    on start {
+      const myTree = newTree(new Foo {
+        foo: 'myFoo',
+        bar: false,
+      });
+      const wrongFoo = new Foo {
+        foo: 'wrongFoo',
+        bar: false,
+      };
+      const myFoo = myTree.getRootNode() || wrongFoo;
+      print(myFoo.foo);
+      emit exit 0;
+    }"#;
+    stdout "myFoo\n";
+);
+test!(tree_every_find_some_reduce_prune => r#"
+    from @std/app import start, print, exit
+
+    on start {
+      const myTree = newTree('foo');
+      const barNode = myTree.addChild('bar');
+      const bazNode = myTree.addChild('baz');
+      const bayNode = barNode.addChild('bay');
+
+      print(myTree.every(fn (c: Node<string>): bool = (c || 'wrong').length() == 3));
+      print(myTree.some(fn (c: Node<string>): bool = (c || 'wrong').length() == 1));
+      print(myTree.find(fn (c: Node<string>): bool = (c || 'wrong') == 'bay').getOr('wrong'));
+      print(myTree.find(fn (c: Node<string>): bool = (c || 'wrong') == 'asf').getOr('wrong'));
+
+      print(myTree.length());
+      myTree.getChildren().eachLin(fn (c: Node<string>) {
+        const n = c || 'wrong';
+        if n == 'bar' {
+          c.prune();
+        }
+      });
+      print(myTree.getChildren().map(fn (c: Node<string>): string = c || 'wrong').join(', '));
+      print(myTree.length());
+
+      myTree.reduce(fn (acc: int, i: Node<string>): int = (i || 'wrong').length() + acc || 0, 0).print();
+      emit exit 0;
+    }"#;
+    stdout r#"true
+false
+bay
+wrong
+4
+baz
+2
+6
+"#;
+);
+test!(subtree_and_nested_tree_construction => r#"
+    from @std/app import start, print, exit
+
+    on start {
+      const bigNestedTree = newTree('foo')
+        .addChild('bar')
+        .getTree()
+        .addChild(newTree('baz')
+          .addChild('quux')
+          .getTree()
+        ).getTree();
+
+      const mySubtree = bigNestedTree
+        .getRootNode()
+        .getChildren()[1]
+        .getOr(newTree('what').getRootNode())
+        .toSubtree();
+
+      print(bigNestedTree.getRootNode() || 'wrong');
+      print(mySubtree.getRootNode() || 'wrong');
+
+      emit exit 0;
+    }"#;
+    stdout "foo\nbaz\n";
+);
