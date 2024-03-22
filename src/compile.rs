494c646534de3d91e650d8e984ef9e7650dbdf1c
David Ellis <isv.damocles@gmail.com>
2024-03-22 14:15:39 -0500

    Compile rust transpile as cargo projects

--- src/compile.rs
+++ src/compile.rs
@@ -1,41 +1,264 @@
-use std::fs::{remove_file, write};
+use std::env::current_dir;
+use std::fs::{create_dir_all, read_to_string, remove_file, write};
 use std::path::PathBuf;
-use std::process::{Command, Stdio};
+use std::process::{Command, Stdio, id};
+use std::thread::sleep;
+use std::time::Duration;
+
+use dirs::config_dir;
+use sysinfo::System;
 
 use crate::lntors::lntors;
 
-/// The `compile` function is a very thin wrapper on top of `lntors`, just handling the file
-/// loading and temporary file storage and removal on the path to generating the binary.
+/// The `compile` function creates a temporary directory that is a Cargo project primarily
+/// consisting of a single source file, plus a Cargo.toml file including the 3rd party dependencies
+/// in the standard library. While this *should* be some configurable thing in the standard library
+/// code instead, the contents of the Cargo.toml are just hardwired in here for now.
 pub fn compile(source_file: String) -> Result<(), Box<dyn std::error::Error>> {
-    // Fail if rustc is not present
+    // Fail if rustc is not present TODO: Present a better error to the user
     Command::new("which").arg("rustc").output()?;
-    // Generate the rust code to compile
-    let rs_str = lntors(source_file.clone())?;
-    // Shove it into a temp file for rustc
-    let tmp_file = match PathBuf::from(source_file).file_stem() {
-        Some(pb) => format!("{}.rs", pb.to_string_lossy().to_string()),
-        None => {
-            return Err("Invalid path".into());
-        }
+    // Also fail if cargo is not present
+    Command::new("which").arg("cargo").output()?;
+    // Because all Alan programs use the same Rust dependencies (for now), we can cut down a *lot*
+    // of build time by re-using the `./target/release/build` and `./target/release/deps` directory
+    // in subsequent builds. Since it takes over 30 seconds to make a release build on my laptop
+    // there needs to be a multi-step process to detect if there's a concurrent build happening
+    // that we should wait for. First we need to look for a `{CONFIG}/alan` directory. If it's not
+    // there, make one, build a guaranteed Hello, World app within it, then use it in the regular
+    // build flow. If it *is* there *and* the `hello_world` application is present, we need to see
+    // if another Alan compile is concurrently running, if so, we sleep wait until it is gone
+    // (either the lockfile is deleted or the process ID in the lockfile is no longer running and
+    // then we delete it and continue. Then we continue with the regular build flow.
+    let config_dir = match config_dir() {
+        Some(c) => Ok(c),
+        None => Err("Somehow no configuration directory exists on this operating system"),
+    }?;
+    let alan_config = { // All this because `push` is not chainable :/
+        let mut a = config_dir.clone();
+        a.push("alan");
+        a
     };
-    write(&tmp_file, rs_str)?;
+    let lockfile = {
+        let mut l = alan_config.clone();
+        l.push(".lockfile");
+        l
+    };
+    let project_dir = {
+        let mut p = alan_config.clone();
+        p.push("alan_generated_bin");
+        p
+    };
+    let release_path = {
+        let mut r = project_dir.clone();
+        r.push("target");
+        r.push("release");
+        r
+    };
+    if alan_config.exists() {
+        if lockfile.exists() {
+            let mut count = 0;
+            while lockfile.exists() && count < 300 {
+                // Check if the pid in the lockfile is still running
+                match read_to_string(lockfile.clone()) {
+                    Err(_) => { // Probably being deleted while we were trying to read, we'll check next time
+                        sleep(Duration::from_secs(1));
+                        count = count + 1;
+                    }, 
+                    Ok(pid) => {
+                        let pid = pid.parse::<u32>().unwrap();
+                        let sys = System::new_all();
+                        let mut found = false;
+                        for (p, _) in sys.processes() {
+                            if p.as_u32() == pid {
+                                found = true;
+                                break;
+                            }
+                        }
+                        if found {
+                            // The process is still running, let's wait
+                            sleep(Duration::from_secs(1));
+                            count = count + 1;
+                        } else {
+                            // The process exited without removing the lockfile, we're taking over
+                            remove_file(lockfile.clone())?;
+                            sleep(Duration::from_secs(1));
+                            return compile(source_file);
+                        }
+                    }
+                }
+            }
+            if count == 300 {
+                panic!("Build initialization failed. Please retry with an active internet connection");
+            }
+            // If we got here, the lockfile shouldn't exist, so let's acquire it
+            write(lockfile.clone(), format!("{}", id()))?;
+        } else {
+            // We'll assume that this has been created correctly and no other process is running
+            write(lockfile.clone(), format!("{}", id()))?;
+        }
+    } else {
+        // First time initialization of the alan config directory
+        create_dir_all(alan_config.clone())?;
+        write(lockfile.clone(), format!("{}", id()))?;
+        match create_dir_all(project_dir.clone()) {
+            Ok(a) => Ok(a),
+            Err(e) => {
+                remove_file(lockfile.clone())?;
+                Err(e)
+            }
+        }?;
+        let cargo_str = r#"[package]
+name = "alan_generated_bin"
+edition = "2021"
+
+[dependencies]
+wgpu = "0.19.3""#;
+        let cargo_path = {
+            let mut c = project_dir.clone();
+            c.push("Cargo.toml");
+            c
+        };
+        match write(cargo_path, cargo_str) {
+            Ok(a) => Ok(a),
+            Err(e) => {
+                remove_file(lockfile.clone())?;
+                Err(e)
+            }
+        }?;
+        let src_path = {
+            let mut s = project_dir.clone();
+            s.push("src");
+            s
+        };
+        match create_dir_all(src_path.clone()) {
+            Ok(a) => Ok(a),
+            Err(e) => {
+                remove_file(lockfile.clone())?;
+                Err(e)
+            }
+        }?;
+        let hello_ln = "export fn main = print('Hello, World!');";
+        let hello_path = {
+            let mut l = src_path.clone();
+            l.push("hello.ln");
+            l
+        };
+        match write(hello_path.clone(), hello_ln) {
+            Ok(a) => Ok(a),
+            Err(e) => {
+                remove_file(lockfile.clone())?;
+                Err(e)
+            }
+        }?;
+        let rs_str = match lntors(hello_path.to_string_lossy().to_string()) {
+            Ok(a) => Ok(a),
+            Err(e) => {
+                remove_file(lockfile.clone())?;
+                Err(e)
+            }
+        }?;
+        let main_path = {
+            let mut m = src_path.clone();
+            m.push("main.rs");
+            m
+        };
+        match write(main_path, rs_str) {
+            Ok(a) => Ok(a),
+            Err(e) => {
+                remove_file(lockfile.clone())?;
+                Err(e)
+            }
+        }?;
+        match remove_file(hello_path) {
+            Ok(a) => Ok(a),
+            Err(e) => {
+                remove_file(lockfile.clone())?;
+                Err(e)
+            }
+        }?;
+        match Command::new("cargo")
+            .current_dir(project_dir.clone())
+            .arg("build")
+            .arg("--release")
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .output() {
+            Ok(a) => Ok(a),
+            Err(e) => {
+                remove_file(lockfile.clone())?;
+                Err(e)
+            }
+        }?;
+    }
+    // Once we're here, the base hello world app we use as a build cache definitely exists, so
+    // let's get to work! We can't use the `?` operator directly here, because we need to make sure
+    // we remove the lockfile on any failure.
+    let src_dir = {
+      let mut s = project_dir.clone();
+      s.push("src");
+      s
+    };
+    // Generate the rust code to compile
+    let rs_str = match lntors(source_file.clone()) {
+        Ok(s) => Ok(s),
+        Err(e) => {
+            remove_file(lockfile.clone())?;
+            Err(e)
+        }
+    }?;
+    // Shove it into a temp file for rustc
+    let rs_path = {
+        let mut r = src_dir.clone();
+        r.push("main.rs");
+        r
+    };
+    match write(rs_path, rs_str) {
+        Ok(a) => Ok(a),
+        Err(e) => {
+            remove_file(lockfile.clone())?;
+            Err(e)
+        }
+    }?;
     // Build the executable
-    Command::new("rustc")
-        .arg("--edition")
-        .arg("2021")
-        .arg("-C")
-        .arg("opt-level=3")
-        .arg(&tmp_file)
+    match Command::new("cargo")
+        .current_dir(project_dir.clone())
+        .arg("build")
+        .arg("--release")
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
-        .output()?;
-    // Drop the temp file
-    remove_file(tmp_file)?;
+        .output() {
+        Ok(a) => Ok(a),
+        Err(e) => {
+            remove_file(lockfile.clone())?;
+            Err(e)
+        }
+    }?;
+    // Copy the binary from the temp directory to the current directory
+    let project_name_path = PathBuf::from(source_file);
+    let project_name_str = match project_name_path.file_stem() {
+        None => panic!("Somehow can't parse the source file name as a path?"),
+        Some(n) => n.to_string_lossy().to_string(),
+    };
+    match Command::new("cp")
+        .current_dir(release_path)
+        .arg("alan_generated_bin")
+        .arg(format!("{}/{}", current_dir()?.to_string_lossy(), project_name_str))
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output() {
+        Ok(a) => Ok(a),
+        Err(e) => {
+            remove_file(lockfile.clone())?;
+            Err(e)
+        }
+    }?;
+    // Drop the lockfile
+    remove_file(lockfile)?;
     Ok(())
 }
 
-/// The `to_rs` function is an even thinner wrapper on top of `lntors` that shoves the output into
-/// a `.rs` file.
+/// The `to_rs` function is an thin wrapper on top of `lntors` that shoves the output into a `.rs`
+/// file.
 pub fn to_rs(source_file: String) -> Result<(), Box<dyn std::error::Error>> {
     // Generate the rust code to compile
     let rs_str = lntors(source_file.clone())?;
@@ -75,8 +298,8 @@ macro_rules! test {
                 let run = super::Command::new(format!("./{}", stringify!($rule))).output()?;
                 $( $type!($test_val, &run); )+
                 // Cleanup the temp files. TODO: Make this happen regardless of test failure?
-                super::remove_file(&filename)?;
-                super::remove_file(stringify!($rule))?;
+                std::fs::remove_file(&filename)?;
+                std::fs::remove_file(stringify!($rule))?;
                 Ok(())
             }
         }
