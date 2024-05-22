2d15f1e849b36425f82633671215689f03464fa8
David Ellis <isv.damocles@gmail.com>
2024-05-22 14:00:24 -0500

    Simplify type definition to just type assignment, no 'structlike' syntax

--- src/lntors/typen.rs
+++ src/lntors/typen.rs
@@ -7,7 +7,7 @@ pub fn generate(
     typen: &Type,
     _scope: &Scope,
     _program: &Program,
-    mut out: OrderedHashMap<String, String>,
+    out: OrderedHashMap<String, String>,
 ) -> Result<(String, OrderedHashMap<String, String>), Box<dyn std::error::Error>> {
     match &typen.typetype {
         // The first value is the identifier, and the second is the generated source. For the
@@ -16,37 +16,8 @@ pub fn generate(
         // output, while the `Structlike` type requires a new struct to be created and inserted
         // into the source definition, potentially inserting inner types as needed
         TypeType::Bind(s) => Ok((s.clone(), out)),
-        TypeType::Alias(a) => Ok((a.to_string(), out)),
-        TypeType::Structlike(_s) => {
-            let mut typestring = format!(
-                r#"#[derive(Debug)]
-struct {} {{
-"#,
-                typen.typename.to_string()
-            );
-            // TODO: Redo all of this to use the built-in function types concept
-            /*for typeline in &s.typelist {
-                        let typename = typeline.fulltypename.to_string();
-                        let (subtypen, subtypescope) = match program.resolve_type(scope, &typename) {
-                            None => Err(format!("Type {} not found", typename)),
-                            Some((t, s)) => Ok((t, s)),
-                        }?;
-                        let res = generate(subtypen, subtypescope, program, out)?;
-                        let subtypename = res.0;
-                        out = res.1;
-                        typestring = format!(
-                            r#"{}
-            pub {}: {},"#,
-                            typestring, typeline.variable, subtypename
-                        );
-                    }*/
-            typestring = format!(
-                r#"{}
-}}"#,
-                typestring
-            );
-            out.insert(typen.typename.to_string(), typestring.to_string());
-            Ok((typen.typename.to_string(), out))
-        }
+        // TODO: The "alias" type is just the normal type assignment path, now, no distinction so
+        // this needs to be more capable going forward
+        TypeType::Create(a) => Ok((a.to_string(), out)),
     }
 }
