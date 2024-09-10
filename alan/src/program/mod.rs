d9ff5ac7a72355422faa5fdf88939c08129a6b87
David Ellis <isv.damocles@gmail.com>
2024-09-10 13:12:14 -0500

    Drop special import syntax from the language

--- alan/src/program/mod.rs
+++ alan/src/program/mod.rs
@@ -4,7 +4,6 @@ mod ctype;
 mod export;
 mod fnkind;
 mod function;
-mod import;
 mod microstatement;
 mod operatormapping;
 #[allow(clippy::module_inception)]
@@ -18,7 +17,6 @@ pub use ctype::CType;
 pub use export::Export;
 pub use fnkind::FnKind;
 pub use function::Function;
-pub use import::Import;
 pub use microstatement::Microstatement;
 pub use operatormapping::OperatorMapping;
 pub use program::Program;
