c4ac3ea58cb35f0e502b00a5719464e2a392b068
David Ellis <isv.damocles@gmail.com>
2024-10-24 15:18:55 -0500

    Add last GPGPU test, actual execution of a shader on the GPU

--- alan_std.test_gpgpu.js
+++ alan_std.test_gpgpu.js
@@ -53,6 +53,24 @@ import { chromium } from 'playwright';
     return v.map((i) => i.valueOf());
   }), [5, 6, 7, 8]);
 
+  assert.deepEqual(await page.evaluate(async () => {
+    let b = await alanStd.createBufferInit(alanStd.storageBufferType(), [2, 2, 2, 2]);
+    let plan = new alanStd.GPGPU(`
+      @group(0)
+      @binding(0)
+      var<storage, read_write> vals: array<i32>;
+
+      @compute
+      @workgroup_size(1)
+      fn main(@builtin(global_invocation_id) id: vec3<u32>) {
+        vals[id.x] = vals[id.x] * i32(id.x);
+      }
+    `, [4, 1, 1], [[b]]);
+    await alanStd.gpuRun(plan);
+    let v = await alanStd.readBuffer(b);
+    return v.map((i) => i.valueOf());
+  }), [0, 2, 4, 6]);
+
   await context.close();
   await browser.close();
 })();
