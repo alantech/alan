832a16148beb101367d262576173fd2b32469e96
David Ellis <isv.damocles@gmail.com>
2024-10-24 15:57:39 -0500

    Oops, I passed the args to the constructor in the wrong order

--- alan_std.test_gpgpu.js
+++ alan_std.test_gpgpu.js
@@ -65,7 +65,7 @@ import { chromium } from 'playwright';
       fn main(@builtin(global_invocation_id) id: vec3<u32>) {
         vals[id.x] = vals[id.x] * i32(id.x);
       }
-    `, [4, 1, 1], [[b]]);
+    `, [[b]], [4, 1, 1]);
     await alanStd.gpuRun(plan);
     let v = await alanStd.readBuffer(b);
     return v.map((i) => i.valueOf());
