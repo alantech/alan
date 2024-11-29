38e116afcdc15c7c2a9bd93421122cc317cb3105
David Ellis <isv.damocles@gmail.com>
2024-11-29 11:40:06 -0600

    There's some weird rendering artifacts on the Pi5, adding back some debug logging to try and figure it out

--- alan_std/src/lib.rs
+++ alan_std/src/lib.rs
@@ -1290,6 +1290,11 @@ impl ApplicationHandler for AlanWindow {
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
+                println!("CloseRequested");
+                use std::io::Write;
+                match std::io::stdout().flush() {
+                    _ => {}
+                }
                 // Cleanup the app now that we're caching things
                 self.buffer_width = None;
                 self.buffer = None;
@@ -1302,6 +1307,11 @@ impl ApplicationHandler for AlanWindow {
                 event_loop.exit();
             }
             WindowEvent::Resized(new_size) => {
+                println!("Resized");
+                use std::io::Write;
+                match std::io::stdout().flush() {
+                    _ => {}
+                }
                 window_gpu_init(self);
                 self.buffer_width = Some(if (4 * new_size.width) % 256 == 0 {
                     4 * new_size.width
@@ -1324,6 +1334,11 @@ impl ApplicationHandler for AlanWindow {
                 self.window.as_ref().unwrap().request_redraw();
             }
             WindowEvent::RedrawRequested => {
+                println!("RedrawRequested");
+                use std::io::Write;
+                match std::io::stdout().flush() {
+                    _ => {}
+                }
                 let start = std::time::Instant::now();
                 // TODO: The setup starting here should not be done on every frame draw
                 let mut size = self.window.as_ref().unwrap().inner_size();
