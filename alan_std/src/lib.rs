3434bcd1a7220f472a707ec8477c37a12ac082bb
David Ellis <isv.damocles@gmail.com>
2024-12-05 10:27:00 -0600

    I don't have vim set up on this MacOS machine, so I didn't catch these mistakes in time

--- alan_std/src/lib.rs
+++ alan_std/src/lib.rs
@@ -1217,7 +1217,7 @@ impl ApplicationHandler for AlanWindow {
                 self.exiting = true;
                 // Cleanup the app now that we're caching things
                 self.buffer_width = None;
-                if let Some(b) = self.buffer {
+                if let Some(b) = &self.buffer {
                     b.destroy();
                 }
                 self.buffer = None;
@@ -1231,7 +1231,7 @@ impl ApplicationHandler for AlanWindow {
             }
             WindowEvent::Resized(_new_size) => {
                 if self.exiting { return; }
-                if let Some(b) = self.buffer {
+                if let Some(b) = &self.buffer {
                     b.destroy();
                 }
                 self.buffer = None;
