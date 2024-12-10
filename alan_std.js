9950a3cf72322f86a8288cd3163d122472272d6c
David Ellis <isv.damocles@gmail.com>
2024-12-09 23:19:23 -0600

    Let's try that again

--- alan_std.js
+++ alan_std.js
@@ -31,13 +31,13 @@ export class FuzzySet {
   store(val) {
     // TODO: Create a 'universal' hash function for JS to make the key
     // TODO: Remove this GPUBuffer hack eventually
-    this.map[val instanceof (window ?? global).GPUBuffer ? val.label : val.toString()] = val;
+    this.map[val instanceof globalThis.GPUBuffer ? val.label : val.toString()] = val;
   }
 
   has(val) {
     return new Bool(
       this.map.hasOwnProperty(
-        val instanceof (window ?? global).GPUBuffer ? val.label : val.toString()
+        val instanceof globalThis.GPUBuffer ? val.label : val.toString()
       )
     );
   }
